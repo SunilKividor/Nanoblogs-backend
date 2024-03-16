e1df8e35ee2044f00e1ca61d822a7a12c715d678
Sunil <sunilkividor@gmail.com>
2024-03-16 23:40:32 +0530

    added cors headers

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,30 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/SunilKividor/internal/auth"
 	"github.com/SunilKividor/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func Router(r *gin.Engine) {
+
+	r.Use(func(c *gin.Context) {
+		// Set CORS headers
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, access-control-allow-origin")
+
+		// If the request method is OPTIONS, handle it and return 200 status code
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		// Continue to the next middleware or route handler
+		c.Next()
+	})
 	//auth
 	r.POST("/auth/login", handlers.Login)
 	r.POST("/auth/signup", handlers.Signup)
