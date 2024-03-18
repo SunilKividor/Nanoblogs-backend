c7f966ce7513c85ebbc1b407e6ebc9a93e884aec
Sunil <sunilkividor@gmail.com>
2024-03-19 02:00:12 +0530

    modified blogs content type to array

--- internal/models/blog_model.go
+++ internal/models/blog_model.go
@@ -8,28 +8,28 @@ import (
 )
 
 type Blog struct {
-	Id         uuid.UUID `json:"id"`
-	UserId     uuid.UUID `json:"user_id" validate:"required"`
-	Title      string    `json:"title" validate:"required"`
-	Content    string    `jsong:"content" validate:"required"`
-	Category   string    `json:"category" validate:"required"`
-	Created_At time.Time `json:"created_at"`
-	Updated_At time.Time `json:"updated_at"`
+	Id         uuid.UUID      `json:"id"`
+	UserId     uuid.UUID      `json:"user_id" validate:"required"`
+	Title      string         `json:"title" validate:"required"`
+	Content    pq.StringArray `jsong:"content" validate:"required"`
+	Category   pq.StringArray `json:"category" validate:"required"`
+	Created_At time.Time      `json:"created_at"`
+	Updated_At time.Time      `json:"updated_at"`
 }
 
 type GetBlogResBody struct {
 	Id         uuid.UUID      `json:"id"`
 	Title      string         `json:"title"`
-	Content    string         `jsong:"content"`
+	Content    pq.StringArray `jsong:"content"`
 	Category   pq.StringArray `json:"category"`
 	Created_At time.Time      `json:"created_at"`
 	Updated_At time.Time      `json:"updated_at"`
 }
 
 type PostBlogReqModel struct {
-	Title    string `json:"title" validate:"required"`
-	Content  string `json:"content" validate:"required"`
-	Category string `json:"category" validate:"required"`
+	Title    string   `json:"title" validate:"required"`
+	Content  []string `json:"content" validate:"required"`
+	Category []string `json:"category" validate:"required"`
 }
 
 type UpdateBlogReqModel struct {
--- internal/repository/postgresql/queries.sql
+++ internal/repository/postgresql/queries.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Blogs(id UUID DEFAULT uuid_generate_v4(),userid UUID,title varchar(255) NOT NULL,content varchar(255) NOT NULL,category varchar(255) NOT NULL,created_at timestamp NOT NULL,updated_at timestamp NOT NULL, PRIMARY KEY(id),CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES Users(id));
+CREATE TABLE Blogs(id UUID DEFAULT uuid_generate_v4(),userid UUID,title varchar(255) NOT NULL,content TEXT[],category varchar(255) NOT NULL,created_at timestamp NOT NULL,updated_at timestamp NOT NULL, PRIMARY KEY(id),CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES Users(id));
 
 CREATE TABLE Users (id UUID DEFAULT uuid_generate_v4() ,name VARCHAR(255) NOT NULL,username VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,refresh_token VARCHAR(255),category TEXT[], PRIMARY KEY(id));
 
